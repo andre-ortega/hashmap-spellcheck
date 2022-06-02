a21be566d119f198cfc43c0368ae11ff26dbaa27
sansantas <ortegaan@oregonstate.edu>
2022-06-01 20:29:18 -0700

    Tracking

---
+++ hashMap.c
@@ -0,0 +1,412 @@
+/*
+ * CS 261 Data Structures
+ * Assignment 5
+ * Name: Andre Ortega
+ * Date: 3/5/2020
+ */
+
+#include "hashMap.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+int hashFunction1(const char* key)
+{
+    int r = 0;
+    for (int i = 0; key[i] != '\0'; i++)
+    {
+        r += key[i]; //simple add
+    }
+    return r;
+}
+
+int hashFunction2(const char* key)
+{
+    int r = 0;
+    for (int i = 0; key[i] != '\0'; i++)
+    {
+        r += (i + 1) * key[i]; //mult w/ shift
+    }
+    return r;
+}
+
+/**
+ * Creates a new hash table link with a copy of the key string.
+ * @param key Key string to copy in the link.
+ * @param value Value to set in the link.
+ * @param next Pointer to set as the link's next.
+ * @return Hash table link allocated on the heap.
+ */
+HashLink* hashLinkNew(const char* key, int value, HashLink* next)
+{
+    HashLink* link = malloc(sizeof(HashLink));
+    link->key = malloc(sizeof(char) * (strlen(key) + 1));
+    strcpy(link->key, key);
+    link->value = value;
+    link->next = next;
+    return link;
+}
+
+/**
+ * Free the allocated memory for a hash table link created with hashLinkNew.
+ * @param link
+ */
+static void hashLinkDelete(HashLink* link)
+{
+
+    free(link->key);
+    free(link);
+}
+
+/**
+ * Initializes a hash table map, allocating memory for a link pointer table with
+ * the given number of buckets.
+ * @param map
+ * @param capacity The number of table buckets.
+ */
+void hashMapInit(HashMap* map, int capacity)
+{
+    map->capacity = capacity;
+    map->size = 0;
+    map->table = (HashLink **)malloc(sizeof(HashLink*) * capacity);
+    for (int i = 0; i < capacity; i++)
+    {
+        map->table[i] = NULL;
+    }
+}
+
+/**
+ * Removes all links in the map and frees all allocated memory. You can use
+ * hashLinkDelete to free the links.
+ * @param map
+ */
+void hashMapCleanUp(HashMap* map)
+{
+    HashLink * current;
+    HashLink * garbage;
+
+    for(int i = 0 ; i < map->capacity ; i++)
+    {
+        current = map->table[i];
+
+        while(current)
+        {
+            garbage = current;
+            current = current->next;
+            hashLinkDelete(garbage);
+        }
+
+    }
+
+    free(map->table);
+    map->size = 0;
+}
+
+/**
+ * Creates a hash table map, allocating memory for a link pointer table with
+ * the given number of buckets.
+ * @param capacity The number of buckets.
+ * @return The allocated map.
+ */
+HashMap* hashMapNew(int capacity)
+{
+    HashMap* map = malloc(sizeof(HashMap));
+    hashMapInit(map, capacity);
+    return map;
+}
+
+/**
+ * Removes all links in the map and frees all allocated memory, including the
+ * map itself.
+ * @param map
+ */
+void hashMapDelete(HashMap* map)
+{
+
+    hashMapCleanUp(map);
+    free(map);
+    map = NULL;
+
+}
+
+/**
+ * Returns a pointer to the value of the link with the given key  and skip traversing as well. Returns NULL
+ * if no link with that key is in the table.
+ * 
+ * Use HASH_FUNCTION(key) and the map's capacity to find the index of the
+ * correct linked list bucket. Also make sure to search the entire list.
+ * 
+ * @param map
+ * @param key
+ * @return Link value or NULL if no matching link.
+ */
+int* hashMapGet(HashMap* map, const char* key)
+{
+    
+
+    int index = abs(HASH_FUNCTION(key) % map->capacity);
+
+    HashLink * iter = map->table[index];
+
+    while(iter)
+    {
+        if(strcmp(iter->key, key) == 0)
+        {
+            return &(iter->value);
+        }
+
+        else
+        {
+            iter = iter->next;
+        }
+    }
+
+    return NULL;
+}
+
+/**
+ * Resizes the hash table to have a number of buckets equal to the given 
+ * capacity (double of the old capacity). After allocating the new table, 
+ * all of the links need to rehashed into it because the capacity has changed.
+ * 
+ * Remember to free the old table and any old links if you use hashMapPut to
+ * rehash them.
+ * 
+ * @param map
+ * @param capacity The new number of buckets.
+ */
+void resizeTable(HashMap* map, int capacity)
+{
+    HashLink * current;
+    HashMap * newMap = hashMapNew(capacity * 2);
+
+                /*
+                HashMap* hashMapNew(int capacity)
+                {
+                    printf("\nmark 0.5 New Map");
+                    HashMap* map = malloc(sizeof(HashMap));
+                    hashMapInit(map, capacity);
+                    printf("\nmark 0.55 End New Map");
+                    return map;
+                }*/
+    
+    for(int i = 0 ; i < map->capacity ; i++)
+    {
+        current = map->table[i];
+
+        while(current)
+        {
+            hashMapPut(newMap, current->key, current->value);
+            current = current->next;
+        }
+    }
+    
+
+    hashMapCleanUp(map);
+
+    map->size = newMap->size;
+    map->table = newMap->table;
+    map->capacity = newMap->capacity;
+
+    free(newMap);
+    
+}
+
+/**
+ * Updates the given key-value pair in the hash table. If a link with the given
+ * key already exists, this will just update the value and skip traversing. Otherwise, it will
+ * create a new link with the given key and value and add it to the table
+ * bucket's linked list. You can use hashLinkNew to create the link.
+ * 
+ * Use HASH_FUNCTION(key) and the map's capacity to find the index of the
+ * correct linked list bucket.
+ * 
+ * @param map
+ * @param key
+ * @param value
+ */
+void hashMapPut(HashMap* map, const char* key, int value)
+{
+
+    if(hashMapContainsKey(map, key))
+    {
+        *hashMapGet(map, key) = value;
+    }
+
+    else
+    {
+        int index = abs(HASH_FUNCTION(key) % map->capacity);
+
+
+        map->table[index] = hashLinkNew(key, value, map->table[index]);
+
+        map->size++;
+        
+        float factor = map->size / map->capacity;
+
+        if(factor > 0.75)
+        {
+            resizeTable(map, map->capacity);
+        }
+    }
+
+        /*
+
+        HashLink* hashLinkNew(const char* key, int value, HashLink* next)
+        {
+            HashLink* link = malloc(sizeof(HashLink));
+            link->key = malloc(sizeof(char) * (strlen(key) + 1));
+            strcpy(link->key, key);
+            link->value = value;
+            link->next = next;
+            return link;
+        } */
+}
+
+/**
+ * Removes and frees the link with the given key from the table. If no such link
+ * exists, this does nothing. Remember to search the entire linked list at the
+ * bucket. You can use hashLinkDelete to free the link.
+ * @param map
+ * @param key
+ */
+void hashMapRemove(HashMap* map, const char* key)
+{
+
+  int index = abs(HASH_FUNCTION(key) % map->capacity);
+
+  HashLink * previous = NULL; 
+
+  HashLink * current = map->table[index];
+  
+  while(current)
+  {
+
+      if(strcmp(current->key, key) == 0)
+      {
+
+          if(previous == NULL)
+          {
+              map->table[index] = previous;
+          }
+
+          else
+          {
+              previous->next = current->next;
+          }
+          
+          hashLinkDelete(current);
+          map->size--;
+      }
+
+      previous = current;
+      current = current->next;
+  }
+}
+
+
+/**
+ * Returns 1 if a link with the given key is in the table and 0 otherwise.
+ * 
+ * Use HASH_FUNCTION(key) and the map's capacity to find the index of the
+ * correct linked list bucket. Also make sure to search the entire list.
+ * 
+ * @param map
+ * @param key
+ * @return 1 if the key is found, 0 otherwise.
+ */
+int hashMapContainsKey(HashMap* map, const char* key)
+{ 
+
+    if(hashMapGet(map, key))
+    {
+        return 1;
+    }
+
+    else
+    {
+        return 0;
+    }
+}
+
+/**
+ * Returns the number of links in the table.
+ * @param map
+ * @return Number of links in the table.
+ */
+int hashMapSize(HashMap* map)
+{
+    return map->size;
+}
+
+/**
+ * Returns the number of buckets in the table.
+ * @param map
+ * @return Number of buckets in the table.
+ */
+int hashMapCapacity(HashMap* map)
+{
+    return map->capacity;
+}
+
+/**
+ * Returns the number of table buckets without any links.
+ * @param map
+ * @return Number of empty buckets.
+ */
+int hashMapEmptyBuckets(HashMap* map)
+{
+    int empty = 0;
+    for(int i = 0 ; i < map->capacity ; i++)
+    {
+        if(map->table[i] == NULL)
+        {
+            empty++;
+        }
+    }
+
+    return empty;
+}
+
+/**
+ * Returns the ratio of (number of links) / (number of buckets) in the table.
+ * Remember that the buckets are linked lists, so this ratio tells you nothing
+ * about the number of empty buckets. Remember also that the load is a floating
+ * point number, so don't do integer division.
+ * @param map
+ * @return Table load.
+ */
+float hashMapTableLoad(HashMap* map)
+{
+    float factor = map->size / map->capacity;
+    return factor;
+}
+
+/**
+ * Prints all the links in each of the buckets in the table.
+ * @param map
+ */
+void hashMapPrint(HashMap* map)
+{
+
+
+    HashLink * link;
+
+    for(int i = 0 ; i < map->capacity ; i++)
+    {
+        printf("\n%d: ", i);
+        
+        link = map->table[i];
+
+        while(link)
+        {
+            printf("key: %s, value: %d --> ", link->key, link->value);
+            link = link->next;
+        }
+
+        printf("(null)\n");
+    } 
+
+}
---
+++ spellChecker.c
@@ -0,0 +1,251 @@
+/*
+ * CS 261 Data Structures
+ * Assignment 5
+ * Name: Andre Ortega
+ * Date: 3/5/2020
+ */
+
+#include "hashMap.h"
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int levenshtein(const char * s, const char * t);
+
+/**
+ * Allocates a string for the next word in the file and returns it. This string
+ * is null terminated. Returns NULL after reaching the end of the file.
+ * @param file
+ * @return Allocated string or NULL.
+ */
+char* nextWord(FILE* file)
+{
+    int maxLength = 16;
+    int length = 0;
+    char* word = malloc(sizeof(char) * maxLength);
+    while (1)
+    {
+        char c = fgetc(file);
+        if ((c >= '0' && c <= '9') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            c == '\'')
+        {
+            if (length + 1 >= maxLength)
+            {
+                maxLength *= 2;
+                word = realloc(word, maxLength);
+            }
+            word[length] = c;
+            length++;
+        }
+        else if (length > 0 || c == EOF)
+        {
+            break;
+        }
+    }
+    if (length == 0)
+    {
+        free(word);
+        return NULL;
+    }
+    word[length] = '\0';
+    return word;
+}
+
+/**
+ * Loads the contents of the file into the hash map.
+ * @param file
+ * @param map
+ */
+void loadDictionary(FILE* file, HashMap* map)
+{
+    char * next = nextWord(file);
+
+    while(next)
+    {
+        hashMapPut(map, next, 0);
+        char * temp = next;
+        next = nextWord(file);
+        free(temp);
+    }
+}
+
+/**
+ * Checks the spelling of the word provded by the user. If the word is spelled incorrectly,
+ * print the 5 closest words as determined by a metric like the Levenshtein distance.
+ * Otherwise, indicate that the provded word is spelled correctly. Use dictionary.txt to
+ * create the dictionary.
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, const char** argv)
+{
+    HashMap* map = hashMapNew(1000);
+
+    FILE* file = fopen("dictionary.txt", "r");
+    clock_t timer = clock();
+    loadDictionary(file, map);
+    timer = clock() - timer;
+    printf("Dictionary loaded in %f seconds\n", (float)timer / (float)CLOCKS_PER_SEC);
+    fclose(file);
+
+    char inputBuffer[256];
+    int quit = 0;
+    while (!quit)
+    {
+        printf("Enter a word or \"quit\" to quit: ");
+        scanf("%s", inputBuffer);
+
+        // Implement the spell checker code here..
+
+        for (int i = 0 ; i < strlen(inputBuffer) ; i++)
+        {
+            inputBuffer[i] = tolower(inputBuffer[i]);
+        } 
+
+        if(hashMapContainsKey(map, inputBuffer))
+        {
+            if(strcmp(inputBuffer, "quit") != 0)
+            {
+                printf("\n%s is spelled correctly\n\n", inputBuffer);
+            }
+        }
+
+        else
+        {
+            printf("\n%s is spelled incorrectly", inputBuffer);
+
+            HashLink * current;
+
+            char * suggestions[5];
+
+            for(int i = 0 ; i < 5 ; i++)
+            {
+                suggestions[i] = malloc(128 * sizeof(char));
+            }
+
+            int array[5];
+            for(int i = 0; i < 5 ; i++)
+            {
+                array[i] = 1000;
+            }
+
+            for(int i = 0; i < map->capacity ; i++)
+            {
+                current = map->table[i];
+
+                while(current)
+                {
+
+                    int distance = levenshtein(current->key, inputBuffer);
+
+                    int max_index = 0;
+
+                    for(int j = 0; j < 5 ; j++)
+                    {
+                        if (array[j] > array[max_index])
+                        {
+                            max_index = j;
+                        }
+                    }
+
+                    if(distance < array[max_index])
+                    {
+                        suggestions[max_index] = current->key;
+                        array[max_index] = distance;
+                    }
+
+                    current = current->next;
+                }
+            }
+
+            printf("\n\nSuggestions: %s, %s, %s, %s, %s\n\n", suggestions[0] , suggestions[1] , suggestions[2] , suggestions[3] , suggestions[4]);
+
+
+            for(int i = 0 ; i < 5 ; i++)
+            {
+                free(suggestions[i]);
+            }
+        } 
+
+        if (strcmp(inputBuffer, "quit") == 0)
+        {
+            quit = 1;
+        } 
+    }
+
+    hashMapDelete(map);
+    return 0;
+}
+
+int levenshtein(const char * s, const char * t)
+{
+    int len1 = strlen(s);
+    int len2 = strlen(t);
+
+    int matrix[len1 + 1][len2 + 1];
+    int i;
+    
+    for(i = 0; i <= len1; i++)
+    {
+        matrix[i][0] = i;
+    }
+
+    for(i = 0; i <= len2; i++)
+    {
+        matrix[0][i] = i;
+    }
+
+    for(i = 1; i <= len1; i++)
+    {
+        int j;
+        char c1;
+
+        c1 = s[i - 1];
+        for(j = 1; j <= len2; j++)
+        {
+            char c2;
+
+            c2 = t[j - 1];
+
+            if(c1 == c2)
+            {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            }
+
+            else
+            {
+                int delete;
+                int insert;
+                int substitute;
+                int minimum;
+
+                delete = matrix[i - 1][j] + 1;
+                insert = matrix[i][j - 1] + 1;
+                substitute = matrix[i - 1][j - 1] + 1;
+                minimum = delete;
+
+                if(insert < minimum)
+                {
+                    minimum = insert;
+                }
+
+                if(substitute < minimum)
+                {
+                    minimum = substitute;
+                }
+
+                matrix[i][j] = minimum;
+            }
+        }
+    }
+
+    return matrix[len1][len2];
+
+}
